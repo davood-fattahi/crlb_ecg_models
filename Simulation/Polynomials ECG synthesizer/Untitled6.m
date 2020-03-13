17901e46a65517eaeaa9b5e01e81a7a04ee1f130
Davood_Fattahi <fattahi.d@gmail.com>
2020-03-13 18:08:29 +0330

    add Jonas Lundgren spline fitter

--- Simulation/Polynomials ECG synthesizer/Untitled6.m
+++ Simulation/Polynomials ECG synthesizer/Untitled6.m
@@ -18,6 +18,7 @@ figure
 subplot(311)
 plot(ecg)
 [ecgM, rrM]=ecgmean(ecg,fs);
+ecgM=ecgM+randn(size(ecgM));
 t=1:size(ecgM(:),1);
 subplot(312)
 plot(t,ecgM)
@@ -26,11 +27,11 @@ afp =ecgfidapprox(ecgM,fs,72./60,'widest');
 afp(3,5)=afp(3,6); afp(:,6)=[];
 hold on
 plot(afp(2,:),ecgM(afp(2,:)),'r*')
-plot(afp(1,:),ecgM(afp(1,:)),'bo')
-plot(afp(3,:),ecgM(afp(3,:)),'bo')
+plot(afp(1,:),ecgM(afp(1,:)),'ro')
+plot(afp(3,:),ecgM(afp(3,:)),'ro')
 
 afp(2,:)=[];
-[p,pcs, pct]=ppolyfit2(t,ecgM,[3 5 5 6 3],afp',fs./200);
+[p,pcs, pct]=ppolyfit2(t,ecgM,[3 7 6 7 3],afp',fs./200);
 [S, SS]=ppolyval(p,pct,1);
 subplot(313)
 plot(t,ecgM);
